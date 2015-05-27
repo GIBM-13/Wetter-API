fa03919bca4b0f3d6ea50467fe10bf311e7183ed
Dominik Müller <Ashura@Baka.local>
2015-05-27 12:37:31 +0200

    Added basic code documentation to main class

--- src/Api.php
+++ src/Api.php
@@ -4,12 +4,24 @@
  * User: Patrick
  * Date: 27.05.2015
  * Time: 12:04
+ * @since 27.05.2015
+ * @author Patrick Reimers, Dominik Müller
+ * @version 0.1
  */
 
 namespace GIBM13\WetterApi;
 
 
+/**
+ * Class Api
+ * @category WheaterAPI
+ * @package GIBM13\WetterApi
+ */
 class Api
 {
 
+    public function __construct()
+    {
+
+    }
 }
